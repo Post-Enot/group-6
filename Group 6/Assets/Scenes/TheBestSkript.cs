0d2c571b4c79b9feeb21cd14daa07fba815806dc
Pirsona <LerouSanchez@yandex.ru>
2024-04-22 21:58:37 +0500

    Мудрец
    Добавил великую стену мудреца

---
+++ Group 6/Assets/Scenes/TheBestSkript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheBestSkript : MonoBehaviour
+{
+    public float speed = 50;
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed);
+    }
+}
